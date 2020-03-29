a632ea86a44c6449ced2aa9657b5927c7d79095e
E-Neo <e-neo@qq.com>
2020-03-29 16:26:51 +0800

    delete main.rs

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use mathtools::algebra::real::Float;
-
-fn main() {
-    let x = Float::with_val(4, 1);
-    let y = Float::with_val(32, 2);
-    let z = Float::with_val(32, x + y);
-    println!("{}", z);
-}
